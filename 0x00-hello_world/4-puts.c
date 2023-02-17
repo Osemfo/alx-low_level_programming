82ffbddb021384c6e9a2a0a6e67bffeac001718c
Osemfo <canyinaku@gmail.com>
2023-02-16 17:04:55 -0800

    Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main-a put function program
+ *Return (0) is a success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle.");
+return (0);
+}

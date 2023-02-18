9cb6657aa86bfb597266b52e1088b9425d99444f
Osemfo <canyinaku@gmail.com>
2023-02-17 19:00:02 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main-digits in base 10
+*Return: always 0 (success)
+*/
+
+int main(void)
+{
+int num;
+for (num = 0; num < 10; numm++)
+putchar((num % 10) + '0');
+putchar('\n');
+return (0);
+}

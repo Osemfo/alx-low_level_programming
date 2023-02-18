cac7186e16a5cee3036cd624eb5e811d15ebcc39
Osemfo <canyinaku@gmail.com>
2023-02-17 19:29:25 -0800

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,3 +2,15 @@
 /**
 *main-print isn base 16
 *Return: always 0 (success)
+*/
+int main(void)
+{
+int num;
+char la;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+for (la = 'a'; la <= 'f'; la++)
+putchar(la);
+putchar('\n');
+return (0);
+}

5d3ff41c81a71aaf9fb544e5b9e0997e155320b0
Osemfo <canyinaku@gmail.com>
2023-02-17 19:41:27 -0800

    task 9' '

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+*main-posible combination of single digits
+*Return: always 0 (success)
+*/
+int main(void)
+{
+int n;
+for (n = 48; n = 58; n++)
+{
+putchar(n);
+if (n != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}

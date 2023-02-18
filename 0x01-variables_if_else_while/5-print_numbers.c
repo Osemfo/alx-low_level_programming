a934c0d22eb2be45424329ca4cbcad04e46ee55e
Osemfo <canyinaku@gmail.com>
2023-02-17 18:49:39 -0800

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main-a program that prints numbers
+*Return: always 0(success)
+*/
+
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+printf("%d", num);
+
+printf("\n");
+
+return (0);
+}

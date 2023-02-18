d3f39dc6a7a2ac5504af8968884f8b06e6dddd77
Osemfo <canyinaku@gmail.com>
2023-02-17 19:15:01 -0800

    7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,10 +7,14 @@ int main(void)
 {
 int num;
 char la;
+
 for (num = 0; num < 10; num++)
 putchar((num % 10) + '0');
+
 for (la = 'a'; la <= 'z'; la++)
 putchar(la);
+
 putchar('\n');
+
 return (0);
 }

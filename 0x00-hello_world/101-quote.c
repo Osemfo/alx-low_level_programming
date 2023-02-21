712545751587b52d99b57e42323e50d6ea337a05
Osemfo <canyinaku@gmail.com>
2023-02-20 18:15:34 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *main-C program that prints a specified texts followed by a new line and sandard error
- *Return: 1 (success)
- */
+*main-C program that prints a specified text
+*Return: 1 (success)
+*/
 int main(void)
 {
-fput("and the piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and the piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }

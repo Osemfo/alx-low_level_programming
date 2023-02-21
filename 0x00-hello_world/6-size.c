200212cb29e481f4b043bb9b26acd64bd2f00ba3
Osemfo <canyinaku@gmail.com>
2023-02-20 18:05:24 -0800

    6th

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,6 @@
  *Return: 0 (success)
  */
 int main(void)
-char;
-int;
-long int;
-long long int;
-float;
 printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(char));
 printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(int));
 printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(long int));

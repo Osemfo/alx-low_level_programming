4c5e940505ce7bca54c4c34b202fd1d46b0eef61
Osemfo <canyinaku@gmail.com>
2023-02-20 16:23:46 -0800

    Task 9 redo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
 *main-posible combination of single digits
 *Return: always 0 (success)
@@ -6,10 +8,10 @@
 int main(void)
 {
 int n;
-for (n = 48; n = 58; n++)
+for (n = 0; n <= 9; n++)
 {
 putchar(n);
-if (n != 57)
+if (n != 9)
 {
 putchar(',');
 putchar(' ');

897bfcfe18d493cc6b3795bf7dad421d34a4a7f0
Osemfo <canyinaku@gmail.com>
2023-02-20 18:07:42 -0800

    sizes.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,6 +4,7 @@
  *Return: 0 (success)
  */
 int main(void)
+{
 printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(char));
 printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(int));
 printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(long int));

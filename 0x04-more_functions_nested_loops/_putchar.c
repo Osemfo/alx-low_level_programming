ede91b2072d17e8ddf7e0944d5f9ae646e59ab54
Osemfo <canyinaku@gmail.com>
2023-02-23 18:52:09 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unisted.h>
+/**
+*_putchar - writes the character c to stdout
+*@c: The character to print
+*Return: 1 (success)
+*on error, -1 is returned, and erno is set appropriately
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}

5ebd52a23aef6558a304ba3cc1ca5bf4e994cb2f
Osemfo <canyinaku@gmail.com>
2023-02-27 17:10:21 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_strlen-returns the lenght of a string
+*@s: sring
+*Return: lenght
+*/
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return (len);
+}

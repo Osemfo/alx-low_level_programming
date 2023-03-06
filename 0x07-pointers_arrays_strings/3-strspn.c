40214dcfe508bceeb1ec3738a9213ae53aa12022
Osemfo <canyinaku@gmail.com>
2023-03-05 18:47:53 -0800

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,20 +9,19 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, n, value, check;
-
-value = 0;
-for (i = 0; s[i] != '\0'; i++)
+unsigned int a = 0, b, t = 0;
+while (accept[a])
 {
-check = 0;
-for (n = 0; accept[n] != '\0'; n++)
+b = 0;
+while (s[b] != 32)
 {
-if (accept[n] == s[i])
+if (accept[a] == s[b])
 {
-value++;
-check = 1;
+t++;
 }
+b++;
 }
+a++;
 }
-return (0);
+return (t);
 }
